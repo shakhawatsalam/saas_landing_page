d1c2fb07004203e0de244fea60928cc22c1f7ecc
unknown <shakhawatsalam726@gmail.com>
2024-09-02 17:23:23 +0530

    logotiker section is almost  done

--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -1,3 +1,44 @@
+"use client";
+import acmeLogo from "@/assets/logo-acme.png";
+import quantumLogo from "@/assets/logo-quantum.png";
+import echoLogo from "@/assets/logo-echo.png";
+import celestialLogo from "@/assets/logo-celestial.png";
+import pulseLogo from "@/assets/logo-pulse.png";
+import apexLogo from "@/assets/logo-apex.png";
+import Image from "next/image";
+import { motion } from "framer-motion";
+
+const images = [
+  { src: acmeLogo, alt: "Acme Logo" },
+  { src: quantumLogo, alt: "Quantum Logo" },
+  { src: echoLogo, alt: "Echo Logo" },
+  { src: celestialLogo, alt: "Celestial Logo" },
+  { src: pulseLogo, alt: "Pulse Logo" },
+  { src: apexLogo, alt: "Apex Logo" },
+  { src: acmeLogo, alt: "Acme Logo" },
+  { src: quantumLogo, alt: "Quantum Logo" },
+  { src: echoLogo, alt: "Echo Logo" },
+  { src: celestialLogo, alt: "Celestial Logo" },
+  { src: pulseLogo, alt: "Pulse Logo" },
+  { src: apexLogo, alt: "Apex Logo" },
+];
 export const LogoTicker = () => {
-  return null;
+  return (
+    <div className="py-8 md:py-12 bg-white">
+      <div className='container'>
+        <div className="flex overflow-hidden [mask-image:linear-gradient(to_right,transparent,black,transparent)]">
+          <div className='flex gap-14 flex-none'>
+            {images.map(({ src, alt }) => (
+              <Image
+                key={alt}
+                src={src}
+                alt={alt}
+                className='flex-none h-8 w-auto'
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
