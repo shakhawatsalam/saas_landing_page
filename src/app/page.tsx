80f685a3d2a66e8c5ddfdcdf93c5469015b643a3
unknown <shakhawatsalam726@gmail.com>
2024-09-02 23:27:16 +0530

    [Testimonials section and Calltoaction section is done]

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import { CallToAction } from "@/sections/CallToAction";
 import { Header } from "@/sections/Header";
 import { Hero } from "@/sections/Hero";
 import { LogoTicker } from "@/sections/LogoTicker";
 import { Pricing } from "@/sections/Pricing";
 import { ProductShowcase } from "@/sections/ProductShowcase";
+import { Testimonials } from "@/sections/Testimonials";
 
 export default function Home() {
   return (
@@ -12,6 +14,8 @@ export default function Home() {
       <LogoTicker />
       <ProductShowcase />
       <Pricing />
+      <Testimonials />
+      <CallToAction />
     </>
   );
 }
